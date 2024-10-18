40a298c6819917537f26d95f31e54bc335ac69c4
vintasoft <alex@vintasoft.com>
2024-10-18 12:17:00 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("VintaSoft WPF Camera Barcode Reader Demo")]
 [assembly: AssemblyDescription("Shows how to capture images images from camera and recognize barcodes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Llc.")]
+[assembly: AssemblyCompany("VintaSoft LLC")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("12.5.6.1")]
-[assembly: AssemblyFileVersion("12.5.6.1")]
+[assembly: AssemblyVersion("12.5.7.1")]
+[assembly: AssemblyFileVersion("12.5.7.1")]
