ee49329341916b4cad65d075228f5eb326d34248
vintasoft <alex@vintasoft.com>
2025-01-17 14:30:50 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VintaSoft LLC")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft LLC 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Windows;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("14.0.2.1")]
-[assembly: AssemblyFileVersion("14.0.2.1")]
+[assembly: AssemblyVersion("14.0.3.1")]
+[assembly: AssemblyFileVersion("14.0.3.1")]
